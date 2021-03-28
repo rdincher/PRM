02009d81349e3dc3201678fbb92ebea70a73cf8f
rdincher <rdincher14@gmail.com>
2021-03-28 17:07:32 -0500

    Convert to Typescript
    Initial conversion

--- backend/src/handler.ts
+++ backend/src/handler.ts
@@ -1,6 +1,9 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export async function viewContact (event:APIGatewayProxyEvent): Promise<APIGatewayProxyResult> 
+{
   return {
     statusCode: 200,
     body: JSON.stringify(
